9f69eb68d9cad4d7e8e68d2cceb36fbf6dea7961
advantageous-overtake <107685378+advantageous-overtake@users.noreply.github.com>
2025-09-01 00:40:52 +0200

    feat: separate screenshot capture and screenshot upload

---
+++ dist/util/screenshot-upload.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+if [ "$(wl-paste | file - | grep -Eo "PNG image data" | wc -l)" != 0 ]; then
+  
+  random_name="$(openssl rand -hex 4).png"
+
+  temporary_file="/tmp/${random_name}"
+
+  wl-paste > "$temporary_file"
+
+  if [ "$( wc -l < "${temporary_file}" )" != 0 ]; then
+    if scp "${temporary_file}" "overtake.dev:/srv/http/static/uploads/${random_name}"; then
+      echo "https://uploads.static.overtake.dev/${random_name}" | wl-copy
+    fi
+  fi
+
+  rm -f "${temporary_file}"
+fi
