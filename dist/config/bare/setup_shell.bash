2b490686d8087547b5f37852f591f7843d291da9
advantageous-overtake <107685378+advantageous-overtake@users.noreply.github.com>
2025-09-01 00:40:51 +0200

    fix(mode/bare): update extension

--- dist/config/bare/setup_shell.bash
+++ dist/config/bare/setup_shell.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -f "$HOME/util/standard_lib.sh" ] && source "$HOME/util/standard_lib.sh"
+[ -f "$HOME/util/standard_lib.bash" ] && source "$HOME/util/standard_lib.bash"
 
 # Fallback to simple shell if `starship` is not installed
 
