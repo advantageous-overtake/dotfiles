b46c2d62286e1b4f8e80aa13a7e9a5a64fddb463
advantageous-overtake <107685378+advantageous-overtake@users.noreply.github.com>
2025-09-01 00:40:52 +0200

    mod(mode/visual): switch to nouveau nVIDIA driver

--- dist/config/visual/setup_desktop.bash
+++ dist/config/visual/setup_desktop.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-xrandr --dpi 84
+xrandr --listmonitors | awk '{ print $4 }' | xargs -I{} xrandr --output "{}" --set "TearFree" "on" --dpi 92
 
 i3 -c "$HOME/.i3wm" &> /dev/null && clear
